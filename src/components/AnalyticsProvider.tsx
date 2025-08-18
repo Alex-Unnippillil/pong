8158f826996b4f11ed78549bdfbc7715d4780270
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-18 14:54:43 -0400

    feat: guard analytics host option

--- src/components/AnalyticsProvider.tsx
+++ src/components/AnalyticsProvider.tsx
@@ -2,12 +2,13 @@
 
 import { useEffect } from 'react'
 import { initAnalytics } from '@/lib/analytics'
+import { env } from '@/lib/env.client'
 
 let initialized = false
 
 export function AnalyticsProvider() {
   useEffect(() => {
-    if (!initialized && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    if (!initialized && env.NEXT_PUBLIC_POSTHOG_KEY) {
       initAnalytics()
       initialized = true
     }
