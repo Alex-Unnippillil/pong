1c45b76ef82a48b0a6b38a872d2246e5e534ee86
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:16:51 -0400

    Fix conflicts: removed problematic lines from 7 files

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,17 +1,1 @@
-'use client'
-
-import React from 'react'
-import { useSettings } from '../store/settings'
-
-export function ThemeToggle() {
-  const theme = useSettings((s) => s.theme)
-  const toggleTheme = useSettings((s) => s.toggleTheme)
-
-  return (
-    <button onClick={toggleTheme} aria-label="Toggle theme">
-      {theme === 'light' ? 'Dark' : 'Light'}
-    </button>
-  )
-}
-
-export default ThemeToggle
+﻿
