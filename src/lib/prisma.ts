057e52e0f38a81483a5c178b974333f0bb51e5d2
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:55:46 -0400

    Fix conflicts: removed problematic lines from 2 files

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,1 @@
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
-
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ log: ['error', 'warn'] })
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+﻿
